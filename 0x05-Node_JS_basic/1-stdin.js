7357dbe13e9c0bd3e2daf1cb4c05bedc1d8b2cb1
Maurice Ngicho <mrcngicho@gmail.com>
2025-06-10 00:08:00 +0300

    Using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
